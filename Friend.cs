e284a2d056dd113c91b28726354973ec7d63a847
Elfz <douglaz.poli@gmail.com>
2018-03-06 00:17:08 -0300

    Update Friend.cs

--- Friend.cs
+++ Friend.cs
@@ -1,5 +1,5 @@
 using System;
-
+ 
 namespace world
 {
     class Friend
@@ -7,19 +7,19 @@ namespace world
         public string name { get; }
         public double latitude { get; }
         public double longitude { get; }
-
+ 
         public Friend(string name, double latitude, double longitude)  
         {
             this.name = name;
             this.latitude = latitude;
             this.longitude = longitude;
         }
-
+ 
         public override string ToString()
         {
             return $"[{this.name} com latitude de {this.latitude:f2} e longitude de {this.longitude:f2}]";
         }
-
+ 
         public bool Equals(Friend friend)
         {
             return (this.latitude == friend.latitude && this.longitude == friend.longitude);
